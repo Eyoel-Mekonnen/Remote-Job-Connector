44b971e5950aa80df8a76c56b80e0e0d34ba218d
root <root@DESKTOP-B19RQ5P>
2024-10-16 12:43:29 +0300

    fixed upload

--- controllers/JobSeeker.js
+++ controllers/JobSeeker.js
@@ -162,10 +162,12 @@ class JobSeeker {
       const file = req.file ? req.file: null;
       const jobId = req.params ? req.params.jobId: null;
       const jobSeekerId = req.jobSeeker ? req.jobSeeker._id: null;
+      /*
       const jobSeekerName = await dbClient.db.collection('JobSeeker').findOne({_id: ObjectId(jobSeekerId)});
       if (!jobSeekerName) {
         return res.status(404).json({error: 'Job Seeker Name not Provided'});
       }
+      */
 
       if (!jobSeekerId) {
         return res.status(400).json({ error: 'No ID provided' });
